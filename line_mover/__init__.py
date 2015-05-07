a123dee5ca5bd96697b810b37467d742aeae2cef
Darius Kellermann <darius.kellermann@gmail.com>
2015-05-07 13:57:07 +0200

    Rewritten line moving fixes issues moving near first or last lines

--- line_mover/__init__.py
+++ line_mover/__init__.py
@@ -50,32 +50,97 @@ class LineMoverPlugin(GObject.GObject, Gedit.WindowActivatable):
 			return False
 		return True
 
-	def raise_line(self, action = None, param = None):
+	def store_selection(self, doc):
+		ins = doc.get_iter_at_mark(doc.get_insert())
+		sel = doc.get_iter_at_mark(doc.get_selection_bound())
+		self._stored_selection = ((
+			ins.get_line(),
+			ins.get_line_offset()
+		),(
+			sel.get_line(),
+			sel.get_line_offset()
+		))
+
+	def restore_selection(self, doc, at = 0):
+		insmark = doc.get_insert()
+		ins = doc.get_iter_at_line_offset(self._stored_selection[0][0] + at,
+				self._stored_selection[0][1])
+		selmark = doc.get_selection_bound()
+		sel = doc.get_iter_at_line_offset(self._stored_selection[1][0] + at,
+				self._stored_selection[1][1])
+		doc.move_mark(insmark, ins)
+		doc.move_mark(selmark, sel)
+
+	def raise_line(self):
 		doc = self.window.get_active_document()
 		doc.begin_user_action()
-		itstart = doc.get_iter_at_mark(doc.get_insert())
-		itstart.set_line_offset(0);
-		itstart.backward_line()
-		itend = doc.get_iter_at_mark(doc.get_insert())
-		itend.set_line_offset(0);
-		line = doc.get_slice(itstart, itend, True)
-		doc.delete(itstart, itend)
-		itend.forward_line()
-		doc.insert(itend, line)
+		self.store_selection(doc)
+
+		insmark = doc.get_insert()
+
+		# get previous line without breaks
+		pv_beg = doc.get_iter_at_mark(insmark)
+		if not pv_beg.backward_line():
+			# first line
+			doc.end_user_action()
+			return
+		pv_end = doc.get_iter_at_mark(insmark)
+		pv_end.set_line_offset(0)
+		pv_end.backward_char()
+		pv = doc.get_slice(pv_beg, pv_end, True)
+		doc.delete(pv_beg, pv_end)
+
+		# get current line without breaks
+		ln_beg = doc.get_iter_at_mark(insmark)
+		ln_beg.set_line_offset(0)
+		ln_end = doc.get_iter_at_mark(insmark)
+		if ln_end.forward_line():
+			# only if it is not the last line, because that is lacking a char
+			ln_end.backward_char()
+		ln = doc.get_slice(ln_beg, ln_end, True)
+		doc.delete(ln_beg, ln_end)
+
+		doc.insert(ln_beg, pv)
+		ln_beg.backward_line()
+		doc.insert(ln_beg, ln)
+
+		self.restore_selection(doc, -1)
 		doc.end_user_action()
 
-	def lower_line(self, action = None, param = None):
+	def lower_line(self):
 		doc = self.window.get_active_document()
 		doc.begin_user_action()
-		itstart = doc.get_iter_at_mark(doc.get_insert())
-		itstart.forward_line()
-		itend = doc.get_iter_at_mark(doc.get_insert())
-		itend.forward_line()
-		itend.forward_line()
-		line = doc.get_slice(itstart, itend, True)
-		doc.delete(itstart, itend)
-		itstart.backward_line()
-		doc.insert(itstart, line)
+		self.store_selection(doc)
+
+		insmark = doc.get_insert()
+
+		# get next line without breaks
+		nx_beg = doc.get_iter_at_mark(insmark)
+		if not nx_beg.forward_line():
+			# last line
+			doc.end_user_action()
+			return
+		nx_end = doc.get_iter_at_mark(insmark)
+		nx_end.forward_line()
+		if nx_end.forward_line():
+			nx_end.backward_char()
+		nx = doc.get_slice(nx_beg, nx_end, True)
+		doc.delete(nx_beg, nx_end)
+
+		# get current line without breaks
+		ln_beg = doc.get_iter_at_mark(insmark)
+		ln_beg.set_line_offset(0)
+		ln_end = doc.get_iter_at_mark(insmark)
+		ln_end.forward_line()
+		ln_end.backward_char()
+		ln = doc.get_slice(ln_beg, ln_end, True)
+		doc.delete(ln_beg, ln_end)
+
+		doc.insert(ln_beg, nx)
+		ln_beg.forward_line()
+		doc.insert(ln_beg, ln)
+
+		self.restore_selection(doc, 1)
 		doc.end_user_action()
 
 # ex:ts=4:
